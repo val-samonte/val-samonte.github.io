9afa1c53e102aef237aea31e40086ed94a5b3264
Val <val.samonte@gmail.com>
2014-11-12 15:35:13 +0800

    ss

--- slider/slider.js
+++ slider/slider.js
@@ -22,7 +22,7 @@ angular.module('vds.slider',[])
           } else if(posx < 0) {
             posx = 0;
           }
-
+          console.log(Math.round(posx*100/scope.step) / 100 * scope.step, Math.round(posx/scope.step) * scope.step);
           scope.mouseX = Math.round(posx*100/scope.step) / 100 * scope.step;
         }
 
