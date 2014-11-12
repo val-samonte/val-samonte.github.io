dcbb8c6810a639c5addadb4d23e8d60f590a5c39
Val <val.samonte@gmail.com>
2014-11-12 17:22:44 +0800

    test

--- slider/slider.js
+++ slider/slider.js
@@ -11,7 +11,7 @@ angular.module('vds.slider',[])
       replace: true,
       template:
         '<ul class="vds-slider" style="width: {{ width }}px; display: inline-block;" ng-mousemove="onMousemove($event)">' +
-          '<li class="vds-hairline" ng-repeat="hair in measures[0].point" style="left: {{ hair }}px;"></li>' +
+          '<li class="vds-hairline" ng-repeat="hair in measures[0].point" style="left: {{ hair*width }}px;">|</li>' +
           '<vds-stop-marker ng-repeat="marker in data" ng-model="marker" root-width="width" root-mouse-x="mouseX"></vds-stop-marker>' +
         '</ul>',
       link: function (scope, elem, attr, ctrl) {
