e9590fc6349f03b94f23ce88dbee0e8ed9ba96d1
Val <val.samonte@gmail.com>
2014-11-12 13:35:06 +0800

    dependencies

---
+++ slider/slider.js
@@ -0,0 +1,51 @@
+angular.module('vds.slider',[])
+
+  .directive('vdsSlider', function ($window) {
+    return {
+      scope: {
+        step: '=',
+        data: '=ngModel',
+        width: '='
+      },
+      replace: true,
+      template:
+        '<ul class="vds-slider" style="width: {{ width }}px; display: inline-block;" ng-mousemove="onMousemove($event)">' +
+          '<vds-stop-marker ng-repeat="marker in data" ng-model="marker" root-width="width" root-mouse-x="mouseX"></vds-stop-marker>' +
+        '</ul>',
+      link: function (scope, elem, attr, ctrl) {
+
+        scope.width = elem[0].clientWidth;
+        scope.onMousemove = function(e) {
+          var bound = elem[0].getBoundingClientRect(), posx = e.pageX - bound.left;
+          if(posx > bound.width) {
+            posx = bound.width;
+          } else if(posx < 0) {
+            posx = 0;
+          }
+          scope.mouseX = posx;
+        }
+
+      }
+    };
+  })
+
+  .directive('vdsStopMarker', function () {
+    return {
+      scope: {
+        data: '=ngModel',
+        rootWidth: '=',
+        rootMouseX: '='
+      },
+      replace: true,
+      template:
+        '<li class="vds-stop-marker" style="left: {{ pos = (dragging? (data.val = rootMouseX / rootWidth) : data.val) * rootWidth }}px">' +
+          '<button type="button" ng-mousedown="dragging = true" ng-mouseup="dragging = false"></button><div class="vds-pin"></div>'+
+        '</li>',
+      link: function (scope, elem, attr, ctrl) {
+
+      }
+    };
+  })
+
+
+
